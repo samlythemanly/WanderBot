effe5da8268d6154883505cdc32d840036b554a2
Bijan Agahi <bijanagahi@yahoo.com>
2019-09-29 20:40:40 -0600

    added logging, added a couple new commands, productionized everything

--- bot.py
+++ bot.py
@@ -6,26 +6,28 @@ import commands
 import logging
 import random
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='wanderbot.log', filemode='a',level=logging.INFO,format='%(asctime)s - %(message)s')
 client = discord.Client()
+
+l = logging.info # Me being lazy.
 
 ######################
 ##       TODO       ##
 ######################
-#    - LOGGING
 #    - DB engine
 #    - Quidditch commands
-#    - Upload File via DM
-#    - Help on individual commands
+#    - Fuzzy command helper?
 #    - More easter eggs?
+#    - Gambling?!
 #    - fuckin everything else
 
 favorite_user = None
 flagged_message = 0
+CHANNEL_BLACKLIST = ['the-lobby'] # Channels that WanderBot is not allowed in at all.
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    l('We have logged in as {0.user}'.format(client))
     await change_status()
 
 @client.event
@@ -35,11 +37,13 @@ async def on_message(message):
         return
 
     # Is this a DM?
-    if message.channel.type == 'private':
+    if str(message.channel.type) == 'private':
+        l(f"\n-------------\nNew DM from {message.author}:")
+        l(message.content)
         return # ignore for now.
 
     # We only care about messages in specific channels
-    if str(message.channel) not in const.ALLOWED_CHANNELS:
+    if str(message.channel) in CHANNEL_BLACKLIST:
         return
 
     # So there's a message in a text channel that we care about.
@@ -71,36 +75,52 @@ async def on_reaction_add(reaction, user):
     if reaction.message.author == client.user: #They reacted to us!
         global favorite_user
         favorite_user = user
-        print(user)
-        print(user.name)
         return
 
 async def parseMessage(message):
     prefix = message.content.split(' ')[0][1:].lower() #Get the first word of the message without the symbol
+    l(f"\n-------------\nCommand from: {message.author} in channel '{str(message.channel)}' - '{prefix}'")
     # Verify it's a valid command
     if prefix in commands.COMMANDS:
+        # First, make sure this command can be run in the current channel. If not, fail silently.
+        ch_flag = False
+        for ch in commands.COMMANDS[prefix]['channels']:
+            if str(message.channel) == ch:
+                ch_flag = True
+                break
+        if not ch_flag:
+            l(f"Channel '{str(message.channel)}' does not allow that command.")
+            return await message.channel.send(f"I'm sorry {message.author.mention}, I can't do that in this channel :( But I can give you a hug!")
         # Make sure the user has permissions
-        if not checkUserRole(prefix,message.author):
-            return await message.channel.send(f"I'm sorry {message.author.mention}, you can't do that")
+        if not authorizeCommand(prefix,message):
+            l(f"User {message.author} does not have permissions to run that command")
+            return await message.channel.send(f"I'm sorry {message.author.mention}, you can't do that! It's not me, it's you.")
         # Handle the 'special' commands
         if prefix == 'reload':
+            l(f"Reloading modules")
             reloadCommands()
             return await message.channel.send("Gotcha boss! 👍")
         if prefix == 'change_status':
+            l("Changing status")
             return await change_status()
         # Okay, pass control to the commands module.
+        l(f"Authorized user {message.author} to run {prefix}. Executing.")
         return await commands.runner(prefix,message)
     else:
-        return await message.channel.send(f"Whoops! Never heard of that command, {message.author.name}. Try again?")
+        l(f"Could not find '{prefix}' in commands list.")
+        return await message.channel.send(f"Whoops! Never heard of that command, {message.author.mention}. Try again?")
 
-def checkUserRole(command, author):
-    if 'Robot' in [str(r) for r in author.roles]:
+def authorizeCommand(command, message):
+    # Dev role gets auto-approved
+    if 'Robot' in [str(r) for r in message.author.roles]:
         return True
-    if commands.COMMANDS[command]['roles'][0] == 'all':
+    # Check the user's role for this command.
+    if commands.COMMANDS[command]['roles'][0] == const.ROLES.ALL:
         return True
     else:
         for role in commands.COMMANDS[command]['roles']:
-            if role in [str(r) for r in author.roles]:
+            if role in [str(r) for r in message.author.roles]:
+                l(f"Matched on user's role: {role}.")
                 return True
     return False
 
@@ -109,7 +129,7 @@ def checkUserRole(command, author):
 async def change_status():
     new_status = random.choice(const.STATUS_LIST).split(': ')
     activity = discord.Activity(name=new_status[1],type=const.ACTION_ENUM[new_status[0]])
-    print(f"New Activity: {new_status}")
+    l(f"New Activity: {new_status}")
     await client.change_presence(activity=activity)
 
 # Hot-reloading libs - FOR DEBUGGING ONLY 
--- commands.py
+++ commands.py
@@ -2,7 +2,10 @@ from importlib import reload ## DEBUGGING
 import random
 import db
 import const
+import logging
+import asyncio
 
+l = logging.info
 
 PRODUCTION_CHANNELS = [
 	'general',
@@ -19,76 +22,100 @@ PRODUCTION_CHANNELS = [
 	'gushers',
 	'staff-has-fun-sometimes'
 	]
-TEST_CHANNELS = ['bot-test',]
+TEST_CHANNELS = ['bot-test','feature-requests','temp']
 SECRET_CHANNEL = ['temp']
 QUIDDITCH = ['the-field']
-ADMIN_ROLE = 'Admin'
-MOD_ROLE = 'Moderator'
+
+ROLES=const.ROLES
 
 COMMANDS = {
-		'reload': {
-				'roles':['none'],
-				'channels':ALL_ALLOWED_CHANNELS
-				},
+	# General Commands for everyone	
 		'help': {
-				'roles':['all'],
-				'channels':ALL_ALLOWED_CHANNELS
+				'roles':[ROLES.ALL],
+				'channels':PRODUCTION_CHANNELS,
+				'hidden':False
 				},
 		'ping': {
-				'roles':['all'],
-				'channels':ALL_ALLOWED_CHANNELS
+				'roles':[ROLES.ALL],
+				'channels':PRODUCTION_CHANNELS,
+				'hidden':False
 				},
 		'coin': {
-				'roles':['all'],
-				'channels':ALL_ALLOWED_CHANNELS
+				'roles':[ROLES.ALL],
+				'channels':PRODUCTION_CHANNELS,
+				'hidden':False
 				},
-		'change_status':{
-				'roles':[ADMIN_ROLE,MOD_ROLE],
-				'channels':ALL_ALLOWED_CHANNELS
-				},
-	   	'favorite': {
-	   			'roles':['none'],
-	   			'channels':ALL_ALLOWED_CHANNELS
-	   			},
-	   	'link': {
-	   			'roles':['none'],
-	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':ALL_ALLOWED_CHANNELS
-	   			},
-	   	'find': {
-	   			'roles':['none'],
-	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':ALL_ALLOWED_CHANNELS
-	   			},
-	   	'activity': {
-	   			'roles':[ADMIN_ROLE],
-	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':ALL_ALLOWED_CHANNELS
-	   			},
 	   	'mock': {
-	   			'roles':['all'],
-	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':SECRET_CHANNEL
+	   			'roles':[ROLES.ALL],
+	   			'channels':PRODUCTION_CHANNELS,
+	   			'hidden':True
 	   			},
 	   	'compliment': {
-	   			'roles':['all'],
-	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':SECRET_CHANNEL
+	   			'roles':[ROLES.ALL],
+	   			'channels':PRODUCTION_CHANNELS,
+	   			'hidden':True
 	   			},
 	   	'convert': {
-	   			'roles':['all'],
+	   			'roles':[ROLES.ALL],
+	   			'channels': PRODUCTION_CHANNELS,
+	   			'hidden':False
+	   			},
+	   	'joke': {
+	   			'roles':[ROLES.ALL],
+	   			'channels': PRODUCTION_CHANNELS,
+	   			'hidden':True
+	   			},
+	   	'halp': {
+	   			'roles':[ROLES.ALL],
+	   			'channels': PRODUCTION_CHANNELS,
+	   			'hidden':True
+	   			},
+	# Admin / Mod commands 
+	   	'change_status':{
+				'roles':[ROLES.ADMIN,ROLES.MOD],
+				'channels':PRODUCTION_CHANNELS,
+				'hidden':False
+				},
+		# 'activity': {
+	 #   			'roles':[ROLES.ADMIN],
+	 #   			'channels':PRODUCTION_CHANNELS,
+	 #   			'hidden':False
+	 #   			},
+	# Dev commands / in development commands
+		'favorite': {
+	   			'roles':[ROLES.DEV],
+	   			'channels':TEST_CHANNELS,
+	   			'hidden':True
+	   			},
+	   	'link': {
+	   			'roles':[ROLES.DEV],
 	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
-	   			'channels':SECRET_CHANNEL
-	   			}
+	   			'channels':TEST_CHANNELS,
+	   			'hidden':False
+	   			},
+	   	'find': {
+	   			'roles':[ROLES.DEV],
+	   			# 'roles':[ADMIN_ROLE,MOD_ROLE],
+	   			'channels':TEST_CHANNELS,
+	   			'hidden':False
+	   			},
+	   	'reload': {
+				'roles':[ROLES.DEV],
+				'channels':TEST_CHANNELS,
+				'hidden':True
+				},
 }
 
 
 # Help Command 
 #	Lists all the commands the user is allowed to run
 async def help(message):
+	l("Running 'help'")
 	# Get the user roles
 	allowed_commands = []
 	for command in COMMANDS:
+		if COMMANDS[command]['hidden']:
+			continue;
 		if COMMANDS[command]['roles'][0] == 'all':
 			allowed_commands.append(command)
 		else:
@@ -97,15 +124,33 @@ async def help(message):
 					allowed_commands.append(command)
 	return await message.channel.send(f"Hi {message.author.mention}, you can run the following commands: {', '.join([f'`{x}`' for x in allowed_commands])}. Remember to add a '!' before the command you want :)")
 
+# Halp Command 
+#	Lists all the hidden commands the user is allowed to run
+async def halp(message):
+	l("Running 'halp'")
+	# Get the user roles
+	allowed_commands = []
+	for command in COMMANDS:
+		if COMMANDS[command]['hidden']:
+			if COMMANDS[command]['roles'][0] == 'all':
+				allowed_commands.append(command)
+			else:
+				for role in COMMANDS[command]['roles']:
+					if role in [str(r) for r in message.author.roles]:
+						allowed_commands.append(command)
+	return await message.channel.send(f"Surprise {message.author.mention}! You can run the following _*SPECIAL*_ commands: {', '.join([f'`{x}`' for x in allowed_commands])}. Remember to add a '!' before the command you want :)")
+
 
 # Ping Pong
 #	Simply reply 'Pong!' to the users message. Mostly used to check if the bot is up.
 async def ping(message):
+	l("Running 'pong'")
 	return await message.channel.send('Pong!')
 
 # Coin Toss
 #	Flips a 'psudo-fair' coin
 async def coin(message):
+	l("Running 'coin'")
 	if random.randint(0,1):
 		await message.channel.send('Heads!')
 	else:
@@ -113,7 +158,9 @@ async def coin(message):
 
 # Favorite User
 #	 Returns the user who last reacted to WanderBot.
+# TODO: Fix this
 async def favorite(message,current_favorite_user):
+	l("Running 'favorite'")
 	if current_favorite_user:
 		return await message.channel.send(f"{current_favorite_user.display_name} is my current favorite person!")
 	else:
@@ -123,6 +170,7 @@ async def favorite(message,current_favorite_user):
 #	First argument is the discordID (int), all other arguments are comma delimeted characterIDs
 #	ex: !link 12345456 24, 25, 26
 async def link(message):
+	l("Running 'link'")
 	parts = message.content.split(' ') #split on space first
 	prefix = parts[0] # The original command. Ignore it
 	discordID = parts[1] # The id of the person to link to.
@@ -149,6 +197,7 @@ async def link(message):
 # Find Character
 #	Searches the db for a character and returns info about them
 async def find(message):
+	l("Running 'find'")
 	parts = message.content.split(' ') #split on space first
 	prefix = parts[0]
 	charID = parts[1]
@@ -163,9 +212,16 @@ async def find(message):
 # Mock the last tagged user's message
 #	Pretty self explanitory
 async def mock(message):
-	user_to_mock = message.mentions[0]
+	l("Running 'mock'")
+	try:
+		user_to_mock = message.mentions[0]
+	except IndexError:
+		return await message.channel.send(f"There's no one tagged to mock! Looks like the only fool here is you, {message.author.mention}!")
 	if not user_to_mock:
 		return
+	if user_to_mock.id == 617172295902822415 and random.choice(range(15)) == 10:
+		return await message.channel.send(f"WANDERBOT IS TOO POWERFUL TO BE MOCKED, MORTAL.")
+
 	async for m in message.channel.history():
 		if m.author == user_to_mock:
 			response = ''
@@ -177,9 +233,11 @@ async def mock(message):
 				else:
 					response += char.upper()
 					flag = 1
-			return await message.channel.send(f'{response} - {m.author.mention}')
+			return await message.channel.send(f'"{response}" - {m.author.mention}')
 
+# Compliment the tagged user.
 async def compliment(message):
+	l("Running 'compliment'")
 	try:
 		user_to_compliment = message.mentions[0]
 	except IndexError:
@@ -187,10 +245,53 @@ async def compliment(message):
 	compliment = random.choice(const.COMPLIMENTS)
 	return await message.channel.send(f"Hey {user_to_compliment.mention}, {compliment}")
 
-# async def welcome(message):
-# 	amelia_id = message.guild.get_member(466988290969436180)
-# 	general_id = message.guild.get_channel(613010715267301401)
-# 	return await general_id.send(f'Happy first day of Fall @everyone! And thanks {amelia_id.mention} for the wardrobe upgrade <3. https://cdnw.nickpic.host/rR5ESD.png')
+# Convert currencies
+#	If 1 argument is given, convert from usd/gbp to wizard
+#	If 3 arguments are given, convert to usd/gbp
+#	Else, return a helpful message
+async def convert(message):
+	l("Running 'convert'")
+	parts = message.content.replace('$','').replace('£','').split(' ')
+	args = len(parts[1:])
+	# Which direction are they trying to convert?
+	if args == 1: # they're trying to convert muggle -> wizard
+		#This is going to get ugly. 
+		base = float(parts[1])
+		gal_usd = round(base/25)
+		gal_gbp = round(base/20.05)
+		t_usd = base%25
+		t_gbp = base%20.05
+		sic_usd = round(t_usd/1.47)
+		sic_gbp = round(t_gbp/1.18)
+		t_usd = t_usd%1.47
+		t_gbp = t_gbp%1.18
+		knut_usd = round(t_usd/0.05)
+		knut_gbp = round(t_gbp/0.04)
+		return await message.channel.send(f"{message.author.mention}, ${base:.2f} is roughly equal to {gal_usd} galleons, {sic_usd} sickles, and {knut_usd} knuts.\nBut if you're from across the pond, then £{base:.2f} is roughly equal to {gal_gbp} galleons, {sic_gbp} sickles, and {knut_gbp} knuts.")
+	if args == 3: # they're trying to convert wizard -> muggle
+		try:
+			gal = int(parts[1])
+			sic = int(parts[2])
+			knut = int(parts[3])
+			usd = round(((gal*25) + (sic*1.47) + (knut*.05))*100)/100
+			gbp = round(((gal*20.05) + (sic*1.18) + (knut*0.04))*100)/100
+			return await message.channel.send(f"{message.author.mention}, {gal} galleons, {sic} sickles, and {knut} knuts is roughly equal to ${usd} or £{gbp}")
+		except IndexError:
+			pass
+	else:
+		return await message.channel.send(f"Hey {message.author.mention}, looks like you might be having trouble with the `convert` command! You can either supply a single number to go from USD/GBP to Wizarding Currency, or you can supply 3 numbers that correspond to Galleons, Sickles, and Knuts, respectively! Try something like `!convert 10 50 1` or `!convert 150`. Have a great day!")
+	
+# Joke - tell a cheezy joke!
+async def joke(message):
+	l("Running 'joke'")
+	joke = random.choice(const.JOKES)
+	await message.channel.send(joke[0])
+	await asyncio.sleep(3)
+	async with message.channel.typing():
+		await asyncio.sleep(3)
+		return await message.channel.send(joke[1])
+
+
 
 
 
@@ -199,7 +300,7 @@ async def compliment(message):
 ###################################################
 
 async def runner(command, message):
-	# If we've made it here, the user is able to run the command and it's valid.
+	# If we've made it here, the user is able to run the command and it's valid and in the right channel.
 	# This part is sketch, but it works.
 	c = command+'(message)'
 	return await eval(c)
